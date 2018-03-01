5185d7775cae3004f2e98edfd7c10c2e97df67b0
cookab1 <31396749+cookab1@users.noreply.github.com>
2018-03-01 11:58:27 -0500

    Update main.c

--- TimerTest/main.c
+++ TimerTest/main.c
@@ -7,12 +7,41 @@
 
 #include <avr/io.h>
 
+#
 
 int main(void)
 {
     /* Replace with your application code */
     while (1) 
     {
+        
     }
 }
+void setup()
+{
+    pinMode(LEDPIN, OUTPUT);
+    // initialize Timer1
+    cli();         // disable global interrupts
+    TCCR1A = 0;    // set TCCR1A register to 0
+    TCCR1B = 0;    // set TCCR1B register to 0 
 
+    // enable Timer1 overflow interrupt:
+    TIMSK1 | = (1 << TOIE1);
+
+    // Set CS10 bit so timer runs at clock speed: (prescale of 256)
+    TCCR1B |= (1 << CS12); // Sets bit CS12 in TCCR1B
+    
+    // This is the start value so that it counts for the exact off time
+    TCNT1 = D5F2; 
+        
+        //D8AB is the hex value at which I want to start when the LED is on
+
+    // enable global interrupts:
+    sei();
+}
+
+ISR(TIMER1_OVF_vect)
+{
+    digitalWrite(LEDPIN, !digitalRead(LEDPIN));
+    // or use: PORTB ^= _BV(PB5);// PB5 =pin 19 is digitalpin 13
+}
