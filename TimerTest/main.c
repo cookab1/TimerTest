1f2f78cdc1095177266dd09cb8d010bef2e66f37
Andy Cook <andyrock.cook@gmail.com>
2018-02-28 20:42:04 -0500

    init commit

---
+++ TimerTest/main.c
@@ -0,0 +1,18 @@
+/*
+ * TimerTest.c
+ *
+ * Created: 2/28/2018 8:13:03 PM
+ * Author : andyr
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
