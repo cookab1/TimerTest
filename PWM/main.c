1b24c76cb608c040973dbc7d3b67fa956614cc1c
Cook <cookab1@appd.appstate.edu>
2018-03-01 15:13:57 -0500

    1 working, 2 maybe

--- PWM/main.c
+++ PWM/main.c
@@ -31,29 +31,32 @@ void setup()
     TCCR1A = 0;    // set TCCR1A register to 0
     TCCR1B = 0;    // set TCCR1B register to 0 
 
-    TIMSK1 |= (1 << TOIE1); // enable Timer1 overflow interrupt:
-
-    // Set CS12 bit so timer runs at clock speed: (pre-scaling of 256)
+	// turn on CTC mode:
+	TCCR1B |= (1 << WGM12);
+	
+    // Set CS12 bit so timer runs at clock speed: (pre-scale of 256)
     TCCR1B |= (1 << CS12); // Sets bit CS12 in TCCR1B
-    
-    // This is the start value at which I want to start when the LED is off
-    TCNT1 = 0xEDB0;  //0xD8AB is the hex value at which I want to start when the LED is on
+	
+    // set compare match register to desired timer count:
+    OCR1A = 0x124F;
         
-
+	// enable timer compare interrupt:
+	TIMSK1 |= (1 << OCIE1A);
+	
     // enable global interrupts:
     sei();
 }
 
-ISR(TIMER1_OVF_vect)
+ISR(TIMER1_COMPA_vect)
 {
     if((PORTB & LED) == 0) {
-		PORTB |= LED;
-		//0xD8AB is the hex value at which I want to start when the LED is on
-		TCNT1 = 0xEDAF; // reload the timer pre-load
+		//PORTB |= LED;
+		// This is the compare/match value when the LED is on
+		OCR1A = 0x124F; // reload the timer pre-load
 	} else {
 		PORTB &= ~LED;
-		// This is the start value at which I want to start when the LED is off
-		TCNT1 = 0xC0EC; // reload the timer pre-load
+		// This is the compare/match value when the LED is off
+		OCR1A = 0x3F12; // reload the timer pre-load
 	}
 	// or use: PORTB ^= _BV(PB5);// PB5 =pin 19 is digitalpin 13
 }
